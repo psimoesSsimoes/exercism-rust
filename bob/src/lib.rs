4f51ec201cf89fe5fd89341f6934acd09fdeb27a
seomis <p.simoes@campus.fct.unl.pt>
2019-11-12 19:03:42 +0000

    [bob completed with superior answer]

--- bob/src/lib.rs
+++ bob/src/lib.rs
@@ -1,20 +1,20 @@
 pub fn reply(message: &str) -> &str {
-	let msg = message.trim_end();
-	if msg.is_empty() {
-		return "Fine. Be that way!"
-	}
+	 let msg = message.trim();
+    let empty = msg.len() == 0;
+    let has_alpha = msg.to_uppercase() != msg.to_lowercase();
+    let yell = has_alpha && msg == msg.to_uppercase();
+    let question = msg.ends_with('?');
 
-	let x = msg.to_ascii_lowercase();
-	if msg == msg.to_uppercase() && msg.ends_with("?") && !(msg.to_ascii_uppercase() == x){
-		return "Calm down, I know what I'm doing!"
-	}
-	if msg.ends_with("?"){
-		return "Sure."
-	}
-	if msg == msg.to_uppercase() && !(msg.to_ascii_uppercase() == x)  {
-		return "Whoa, chill out!"
-	}
-
-	"Whatever."
+    if empty {
+        "Fine. Be that way!"
+    } else if yell && question {
+        "Calm down, I know what I'm doing!"
+    } else if yell {
+        "Whoa, chill out!"
+    } else if question {
+        "Sure."
+    } else {
+        "Whatever."
+    }
 }
 
