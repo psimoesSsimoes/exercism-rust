15a5265a940b59fc5c78e2ec920d5574d30fe516
seomis <p.simoes@campus.fct.unl.pt>
2019-11-13 10:45:05 +0000

    [matching-brackets] completed and improved

--- matching-brackets/src/lib.rs
+++ matching-brackets/src/lib.rs
@@ -1,50 +1,26 @@
-const B : char = '[';
-const F : char ='{';
-const D : char ='(';
-const BC : char =']';
-const FC : char ='}';
-const DD : char =')';
-
-
 pub fn brackets_are_balanced(string: &str) -> bool {
-	let mut control = vec![];
+	let mut stack = vec![];
 
-	for c in string.chars(){
-		if c == B || c == F || c == D{
-			control.push(c);
-		}
-
-		if c == BC || c == FC || c == DD{
-			let x = control.pop();
-			if x.is_none(){
-				return false
-			}
-
-			if !closes(x.unwrap(),c){
-				return false
-			}
-		}
-	}
-
-	if control.len()!=0{
-		return false
-	}
-
-	true
-}
-
-fn closes(open: char, close :char) -> bool{
-	if open == B{
-		return close == BC
-	}
-
-	if open == F{
-		return close == FC
-	}
-
-	if open == D{
-		return close == DD
-	}
-
-	false
+	for c in string.chars() {
+        match c {
+            '[' | '{' | '(' => stack.push(c),
+            ']' => {
+                if stack.pop() != Some('[') {
+                    return false;
+                }
+            }
+            '}' => {
+                if stack.pop() != Some('{') {
+                    return false;
+                }
+            }
+            ')' => {
+                if stack.pop() != Some('(') {
+                    return false;
+                }
+            }
+            _ => (),
+        }
+    }
+    stack.is_empty()
 }
