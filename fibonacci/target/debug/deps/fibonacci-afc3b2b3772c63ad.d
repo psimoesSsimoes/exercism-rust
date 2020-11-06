8123ff9701a1021aea612bffb090f2a4fe5cf5bd
seomis <ppsimoes@worten.pt>
2020-11-06 09:30:08 +0000

    [WDT-XXX] fibonacci

--- fibonacci/target/debug/deps/fibonacci-afc3b2b3772c63ad.d
+++
@@ -1,5 +0,0 @@
-/home/seomis/exercism-rust/fibonacci/target/debug/deps/fibonacci-afc3b2b3772c63ad: src/main.rs
-
-/home/seomis/exercism-rust/fibonacci/target/debug/deps/fibonacci-afc3b2b3772c63ad.d: src/main.rs
-
-src/main.rs:
--- fibonacci/target/debug/fibonacci.d
+++
@@ -1,1 +0,0 @@
-/home/seomis/exercism-rust/fibonacci/target/debug/fibonacci: /home/seomis/exercism-rust/fibonacci/src/main.rs
